9db2db6ed3ee486d967aae22a2321d58f2874906
Kalpana-Bachkar <kalpanabachkar@gmail.com>
2026-04-05 17:32:43 +0530

    change for localhost

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -121,7 +121,8 @@ export const logout = asyncHandler(async (req, res) => {
         });
     const options = {
         httpOnly: true,
-        secure: true
+        secure: false,
+        sameSite: "None"
     }
     return res.clearCookie('accessToken', options)
         .clearCookie('refreshToken', options)
@@ -148,7 +149,8 @@ export const refreshAccessToken = asyncHandler(async (req, res) => {
         }
         const options = {
             httpOnly: true,
-            secure: true
+            secure: false,
+            sameSite: "None"
         }
 
         const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
